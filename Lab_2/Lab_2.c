9fbe353b23cb7639e56211d040d946134980dae0
ReporterNV <maxima.xa@ya.ru>
2019-09-30 00:55:15 +0700

    dont work commit :-(

--- Lab_2/Lab_2.c
+++ Lab_2/Lab_2.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+typedef struct Note{
+char *note 
+unsigned int numbers;
+struct Note *next;
+
+}Note;
 
 
+void inp_str(){
+}
 
 
-void main(){
+void out_str(){
+}
+
+*/
+int main(){
+
 unsigned int N = 0;
-scanf("%d", &N);
 
-printf("%d", N);
+printf("\n Enter number of strings: ");
+scanf("%u", &N);
 
+if(N>0)
 
+printf("%d\n", N);
+char *mass = malloc(sizeof(char[255])*N );
+
+for(int i = 0; i < N; i++)
+scanf("%s", &mass[i]);
+
+for(int i = 0; i < N; i++)
+printf("%s", (char*)mass[i]);
+
+return 0;
 }
