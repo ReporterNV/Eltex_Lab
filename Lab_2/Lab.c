fe2b3792846786599594fb813bad6d275d30cf8e
ReporterNV <maxima.xa@ya.ru>
2019-10-20 02:29:24 +0700

    rewrite on better version start

--- Lab_2/Lab.c
+++ Lab_2/Lab.c
@@ -18,19 +18,16 @@ int num_in_str(char *p)
 		num_in_ln++;
 return num_in_ln;
 }
-
+*/
 void inp_str()
 {
     printf("\n Enter number of strings: ");
     scanf("%u", &N);
 
     if (abs(!N)) {
-	printf("\nNo lines\n");
+	fprintf(stderr,"\nNo lines\n");
 }
 
-    mass = (char ***) calloc(2, sizeof(char **));
-    z[0] = (char **) calloc(N, sizeof(char *));
-    z[1] = (char **) calloc(N, sizeof(char *));
     
     char buff[MAX_LEN];
 
@@ -43,11 +40,11 @@ void inp_str()
     return z;
 }
 
-void out_str(char **p) {
-	for (int i = 0; i < N; i++)
-	   printf("%s\n", p[i][0]);
+void output() {
+	for (int i = 0; i < 2; i++)
+	   printf("%s\n", mass[i][0]);
 }
-
+/*
 int fsort(const void *a, const void *b) {
 	if( ( (char**) a ) < (char**) b) {
 	    counter++;
@@ -70,18 +67,28 @@ int main(int agvc, char *argv[]) {
 	mass = (char ***)calloc(2, sizeof(char**));
 	mass[0] = (char **)calloc(2, sizeof(char*));
 	mass[1] = (char **)calloc(2, sizeof(char*));
-mass[0][1] = (char*)malloc(sizeof(char)*strlen("1aaa1"));
-mass[0][2] = (char*)malloc(sizeof(char)*strlen("222222b"));
+mass[0][0] = (char*)malloc(sizeof(char)*strlen("1aaa1"));
+mass[1][0] = (char*)malloc(sizeof(char)*strlen("222222b"));
+strcpy( mass[0][0] , "1aaa1");
+strcpy( mass[1][0] , "22222b");
 
+mass[0][1] = (char*)malloc(sizeof(char));
+sprintf(mass[0][1], "%lu", strlen("1aaa1"));
 mass[1][1] = (char*)malloc(sizeof(char));
-sprintf(mass[1][1], "%lu", strlen("1aaa100000"));
-mass[1][2] = (char*)malloc(sizeof(char));
-sprintf(mass[1][2], "%lu", strlen("222222b"));
+sprintf(mass[1][1], "%lu", strlen("222222b"));
 
-if(mass[1][1]<mass[1][2]){
+printf("%s\n", mass[0][1]);
 printf("%s\n", mass[1][1]);
+
+output();
+
+/*
+if(mass[0][1][0]<mass[1][1][0])
+puts("DONE");	
+*/	
+/*printf("%s\n", mass[1][1]);
 printf("%s\n", mass[1][2]);
-}
+}*/
 for(int i=0;i<2;i++){
 free(mass[0][i]);
 free(mass[1][i]);
@@ -89,7 +96,8 @@ free(mass[1][i]);
 	//rewrite to use 2 arrays
 	
 	free(mass);
-/*	inp_str();
+/*	
+ 	inp_str();
 
 	if (mass == 0) {
 	    puts("\nERROR\n");
