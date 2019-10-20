aef60e7f6b6b0c0f89e0e5aa0827cadf03da9647
ReporterNV <maxima.xa@ya.ru>
2019-10-20 20:36:15 +0700

    FINAL

--- Lab_2/Lab.c
+++ Lab_2/Lab.c
@@ -6,86 +6,85 @@
 static int N = 0;
 static unsigned int counter = 0;
 
-
 char ***mass = NULL;
 
 int num_in_str(char *p)
 {
-    int num_in_ln = 0;
-    unsigned short str_len = strlen(p);
-    for (int i = 0; i < str_len; i++)
-	if((p[i]<='9')&&(p[i]>='0'))	
-		num_in_ln++;
-return num_in_ln;
+	int num_in_ln = 0;
+	unsigned short str_len = strlen(p);
+	for (int i = 0; i < str_len; i++)
+		if ((p[i] <= '9') && (p[i] >= '0'))
+			num_in_ln++;
+	return num_in_ln;
 }
 
 void input()
 {
-    printf("\n Enter number of strings: ");
-    scanf("%u", &N);
+	printf("\n Enter number of strings: ");
+	scanf("%u", &N);
 
-    if (abs(!N)) {
-	fprintf(stderr,"\nNo lines\n");
-}
-puts("");
-	mass = (char ***)calloc(N, sizeof(char**));
-    
-    char buff[MAX_LEN];
-
-    for (int i = 0; i < N; i++) {
-	scanf("%s", buff);
-
-	mass[i] = (char **)calloc(2, sizeof(char*));
-	
-	mass[i][0] = (char *) malloc(sizeof (char) * strlen(buff));
-	strcpy(mass[i][0], buff);
-        mass[i][1]= (char*)malloc(sizeof(char));	
-	sprintf(mass[i][1], "%d", num_in_str(buff));
-    
-   
-    }
-
-}
-
-void output() {
-	for (int i = 0; i < N; i++)
-	   printf("%s:%s\n", mass[i][0], mass[i][1]);
-}
-
-int fsort(const void *a, const void *b) {
-register char** el1 = *(char ***)a;
-register char** el2 = *(char ***)b;
-	
-	printf("\n%d(%c):%d(%c)\n", el1[1][0], el1[1][0], el2[1][0], el2[1][0]);
-if( strcmp(( el1[1] ), (el2[1])) < 0) {//What do you think about rewrite this?
-
-	counter++;
-	    return 1;
+	if (abs(!N)) {
+		fprintf(stderr, "\nNo lines\n");
 	}
-	else 
-	return -1;
-    }
+	puts("");
+	mass = (char ***) calloc(N, sizeof (char **));
 
-void free_all(){
+	char buff[MAX_LEN];
 
-    for (int i = 0; i < N; i++) {
-	 free(mass[i][0]);
-	 free(mass[i][1]);
+	for (int i = 0; i < N; i++) {
+		scanf("%s", buff);
 
-free(mass[i]);
+		mass[i] = (char **) calloc(2, sizeof (char *));
+
+		mass[i][0] = (char *) malloc(sizeof (char) * strlen(buff));
+		strcpy(mass[i][0], buff);
+		mass[i][1] = (char *) malloc(sizeof (char));
+		sprintf(mass[i][1], "%d", num_in_str(buff));
+
+	}
+
 }
 
+void output()
+{
+	for (int i = 0; i < N; i++)
+		printf("%s:%s\n", mass[i][0], mass[i][1]);
 }
 
+int fsort(const void *a, const void *b)
+{
+	register char **el1 = *(char ***) a;
+	register char **el2 = *(char ***) b;
 
-int main(int agvc, char *argv[]) {
-input();
+	if (strcmp((el1[1]), (el2[1])) < 0) {	//What do you think about rewrite this?
 
-qsort(mass, N, sizeof(char **), fsort);
-	
-output();
-printf("\nPermutation: %d\n", counter);
-	free_all();	
+		counter++;
+		return 1;
+	} else
+		return -1;
+}
+
+void free_all()
+{
+
+	for (int i = 0; i < N; i++) {
+		free(mass[i][0]);
+		free(mass[i][1]);
+
+		free(mass[i]);
+	}
+
+}
+
+int main(int agvc, char *argv[])
+{
+	input();
+
+	qsort(mass, N, sizeof (char **), fsort);
+
+	output();
+	printf("\nPermutation: %d\n", counter);
+	free_all();
 	free(mass);
 	return 0;
- }
+}
--- Lab_2/gen.c
+++ Lab_2/gen.c
@@ -4,24 +4,31 @@
 
 int main(int argc, char *argv[])
 {
-    srand(time(NULL));
-    unsigned int num = rand() % 100 + 2 ;
-    if (argc > 1)
-	num = atoi(argv[1]);
-    FILE *file = fopen("custom.txt", "w");
-    fprintf(file, "%u\n", num);
+	srand(time(NULL));
+	unsigned int num;
+	int max_len;
+	if (argc > 1) {
+		num = atoi(argv[1]);
+		if (argc > 2) {
+			max_len = atoi(argv[2]);
+		}
+	} else
+		num = rand() % 100 + 2;
 
-    for (unsigned i = 0; i < num; i++) {
-	int k = rand() % 1020 + 1;
-	for (int j = 0; j < k; j++) {	// gen str
-	    char ch = rand() % 42 + 48;
-	    while ((57<ch) && (ch < 65))
-	    	    ch = rand() % 42 + 48;
-	    fprintf(file, "%c", ch);
+	FILE *file = fopen("custom.txt", "w");
+	fprintf(file, "%u\n", num);
+
+	for (unsigned i = 0; i < num; i++) {
+		int k = rand() % max_len + 1;
+		for (int j = 0; j < k; j++) {	// gen str
+			char ch = rand() % 42 + 48;
+			while ((57 < ch) && (ch < 65))
+				ch = rand() % 42 + 48;
+			fprintf(file, "%c", ch);
+		}
+		fprintf(file, "\n");
 	}
-	fprintf(file, "\n");
-    }
 
-    fclose(file);
-return 0;
+	fclose(file);
+	return 0;
 }
