5fe1c872df4226026123fb2e8566d529c293dba9
ReporterNV <maxima.xa@ya.ru>
2019-10-22 20:31:35 +0700

    Lab4 add rename file

--- Lab_4/Lab.c
+++ Lab_4/Lab.c
@@ -5,28 +5,32 @@
 int main(int argc, char *argv[])
 {
 
-	if (argc == 3) {
+/*	if (argc == 3) {
 		FILE *in = NULL;
 		if (in = fopen(argv[1], "rt") == NULL) {
 			puts("Cannot opent the file:%s", argv[1]);
 			return 2;
 		} else {
 			puts("\nIncorrect argsi. Use ./Lab str.txt c\n");
-			return(1);
+			return (1);
 		}
+*/
+		char *a  = argv[1];
+		if((a = strrchr(argv[1],'.'))==NULL)
+			strncat(argv[1],".out", 3);
+		else
+			strcpy(a,".out");
+		/*
+		   char *a = NULL;
+		   while(fscanf(in, "%s", a) != NULL){
+		   for(int i = 0; i < strlen(a))
+		   if(s[i] == argv[2])
 
-char *a = NULL;
-while(fscanf(in, "%s", a) != NULL){
-for(int i = 0; i < strlen(a))
-	if(s[i] == argv[2])
-			
+		   }
 
 
 	}
-
-
-
-}
-
-
+		 */
+	printf("%s", argv[1]);
+return 0;
 }
