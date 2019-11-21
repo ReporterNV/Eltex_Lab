a426c4ff5b80bf400e37f84995f86ff62a19a4a3
ReporterNV <maxima.xa@ya.ru>
2019-11-22 00:11:54 +0700

    start Lab8

--- Lab_8/msg.h
+++ Lab_8/msg.h
@@ -1,18 +1,9 @@
 
+#include<string.h>
 #include<time.h>
 #include<sys/ipc.h>
 #include<sys/msg.h>
 #include<sys/errno.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<wait.h>
-#include<sys/stat.h>
-#include<sys/stat.h>
-#define MAX_SIZE 1024
-
 
 #define MSGPERM 0600    // msg queue permission
 #define MSGTXTLEN 128   // msg text length
