397a1f5eb98dd85c7d5abe1e784eb3bf36ec262e
ReporterNV <maxima.xa@ya.ru>
2019-11-24 17:19:18 +0700

    add worked Lab.c

--- Lab_8/Lab.c
+++ Lab_8/Lab.c
@@ -1,16 +1,39 @@
 #include"msgq.h"
 int main(int argc, char *argv[])
 {
-	int msqid, key;
-	if((msqid = msgget(IPC_PRIVATE, IPC_CREAT | IPC_EXCL))<0){
-		printf(stderr, "ERROR: cannot create message queue.\n");
+//	struct msqid_ds info;
+	int msqid;
+	
+	key_t key = ftok("./Lab.c", 'M');
+	printf("\nKEY: %d\n", key);
+	
+	if ((msqid = msgget(key, MSGPERM | IPC_CREAT | IPC_EXCL)) < 0) {
+		fprintf(stderr, "ERROR: cannot create message queue.\n");
 		return -1;
 	}
-	
-	msg mymassage = {1, "TEST"};
+	printf("\nMS Q created\n");
+	struct msg_buf mymassage = { 1, "TEST" };
+	struct msg_buf MSG;
+	printf("N:%d; MSG:%s\n", mymassage.mtype, mymassage.mtext);
 
-	if(msgsnd(msqid, &mymassage, ,IPC_NOWAIT))
-	
+	int send = msgsnd(msqid, &mymassage, sizeof (msg) - sizeof (long), IPC_NOWAIT);
+	if (send < 0) {
+		perror(strerror(errno));
+		return -2;
+	} else {
+		printf("Message sended\n");
+	}
 
+	msgrcv(msqid, &MSG, 1, MSG.mtype, IPC_NOWAIT);
+	printf("TAKED MSG: %s\n", MSG.mtext);
+
+	send = msgctl(msqid, IPC_RMID, NULL);
+	if (send < 0) {
+		perror(strerror(errno));
+		printf("msgctl (return queue) failed, ID: %d\n", send);
+		return 1;
+	}
+
+	printf("message queue %d is gone\n", msqid);
 	return 0;
 }
