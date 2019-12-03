2b4d928145c8f366bdc7184efc6b22d3c8dce735
Xabirov_Max <Xabirov.2018@nstu.ru>
2019-12-03 17:05:57 +0700

    add: like final but without spaces

--- Lab_8/Lab.c
+++ Lab_8/Lab.c
@@ -1,137 +1,121 @@
 #include"msgq.h"
 
-int
-function (int a, int b)
+int function(int a, int b)
 {
-  while (a != b)
-    {
-      if (a > b)
-	a = a - b;
-      else
-	b = b - a;
-    }
-  return a;
+	while (a != b) {
+		if (a > b)
+			a = a - b;
+		else
+			b = b - a;
+	}
+	return a;
 }
 
-//if(a ==function(a))
-//      printf("%d", a);
-int
-main (int argc, char *argv[])
+int SimpleCheck(int a)
+{
+	for (int i = 2; i < a; i++) {
+		int p = function(i, a);
+		if ((p != a) && (p != 1))
+			return 0;
+	}
+	return a;
+}
+
+int main(int argc, char *argv[])
 {
 
-  key_t key = ftok (".", 'S');
-  printf ("\nKEY: %d\n", key);
+	key_t key = ftok(".", 'S');
+	printf("\nKEY: %d\n", key);
 
-  int msqid;
-
-  if ((msqid = msgget (key, MSGPERM | IPC_CREAT | IPC_EXCL)) < 0)
-    {
-      fprintf (stderr, "\n(Server)ERROR: cannot create message queue.\n");
-      return -1;
-    }
-  fprintf (stderr, "\nMessage queue create\n");
-
-  int threads = 0;
-
-  printf ("\nEnter number threads:");
-  if ((scanf ("%d", &threads) != 1) || (threads <= 0)
-      || (threads > MAX_THREADS))
-    {
-      fprintf (stderr, "Uncorect size\n");
-      fprintf (stderr, "\n(Server)Received exit message\n");
-      if (msgctl (msqid, IPC_RMID, NULL))
-	{
-	  perror (strerror (errno));
-	  return -2;
+	int msqid;
+	if ((msqid = msgget(key, MSGPERM | IPC_CREAT | IPC_EXCL)) < 0) {
+		fprintf(stderr, "\n(Server)ERROR: cannot create message queue.\n");
+		return -1;
 	}
-      fprintf (stderr, "\n(Server)Message queue closed\n");
-      return -1;
-    }
 
-  printf ("Enter min: ");
-  int x = 0;
-  if ((scanf ("%d", &x) != 1) || (x > MAX_SIZE) || (x < 0))
-    {
-      fprintf (stderr, "Uncorect size\n");
-      fprintf (stderr, "\n(Server)Received exit message\n");
-      if (msgctl (msqid, IPC_RMID, NULL))
-	{
-	  perror (strerror (errno));
-	  return -2;
+	fprintf(stderr, "\nMessage queue was created\n");
+
+	printf("\nEnter number threads:");
+	int threads = 0;
+	if ((scanf("%d", &threads) != 1) || (threads <= 0) || (threads > MAX_THREADS)) {
+		fprintf(stderr, "Uncorect size\n");
+		if (msgctl(msqid, IPC_RMID, NULL)) {
+			perror(strerror(errno));
+			return -2;
+		}
+		return -1;
 	}
-      fprintf (stderr, "\n(Server)Message queue closed\n");
-      return -1;
-    }
 
-  printf ("Enter max: ");
-  int y = 0;
-  if ((scanf ("%d", &y) != 1) || (y > MAX_SIZE) || (y < 0))
-    {
-      fprintf (stderr, "Uncorect size\n");
-      fprintf (stderr, "\n(Server)Received exit message\n");
-      if (msgctl (msqid, IPC_RMID, NULL))
-	{
-	  perror (strerror (errno));
-	  return -2;
+	printf("Enter min: ");
+	int min = 0;
+	if ((scanf("%d", &min) != 1) || (min > MAX_SIZE) || (min < 0)) {
+		fprintf(stderr, "Uncorect size\n");
+		if (msgctl(msqid, IPC_RMID, NULL)) {
+			perror(strerror(errno));
+			return -2;
+		}
+		return -1;
 	}
-      fprintf (stderr, "\n(Server)Message queue closed\n");
-      return -1;
-    }
 
-int i = 0;	
-for(i;i<(threads); i++){
-	printf("\n%d-%d\n", x + (y - x)/(threads) * (i), x + (y - x)/(threads) * (i+1)-1);
-}
-if((x+(y-x)/(threads*threads)-1) != y)
-	printf("\n%d-%d\n", x+(y-x)/(threads)*(threads), y);
-
-
-  pid_t pid[MAX_THREADS];
-  i = 0;
-
-  for (int j = x; j < y; j++)
-    {
-      pid[i] = fork ();
-      if (pid[i] < 0)
-	{			//error
-	  printf ("PID ERROR");
-	  return -3;
+	printf("Enter max: ");
+	int max = 0;
+	if ((scanf("%d", &max) != 1) || (max > MAX_SIZE) || (max < min)) {
+		fprintf(stderr, "Uncorect size\n");
+		if (msgctl(msqid, IPC_RMID, NULL)) {
+			perror(strerror(errno));
+			return -2;
+		}
+		return -1;
 	}
-      else if (pid[i] == 0)
-	{			//child
-	  printf ("Child process #%lu start work\n", getpid ());
-	  //if ((strstr(buff, search) > 0))
-	  return j;
+
+	int i = 0;
+
+/*	for (i; i < (threads); i++) {
+		printf("\n%d-%d\n", x + (y - x) / (threads) * (i), x + (y - x) / (threads) * (i + 1) - 1);
 	}
-      i++;
+	if ((x + (y - x) / (threads * threads) - 1) != y)
+		printf("\n%d-%d\n", x + (y - x) / (threads) * (threads), y);
+*/
 
-    }
-  
-  int stat = 0;
-  int status = 0;
+	pid_t pid[MAX_THREADS];
+	i = 0;
+	if ((max - min) < threads)
+		for (int j = min; j <= max; j++) {
+			pid[j] = fork();
+			if (pid[j] < 0) {	//error
+				printf("PID ERROR");
+				return -3;
+			} else if (pid[j] == 0) {	//child
+				printf("Child process #%i start work\n", getpid());
+				return SimpleCheck(j);
+			}
+			i++;
+		}
+/*	else
+		for() 
+*/		
 
-  for (int j = x; j < y; j++)
-    {
-      status = waitpid (pid[j], &stat, 0);
-      if (status == pid[j])
-	{
-	  if (WEXITSTATUS (stat) == -3)
-	    {
-	      printf ("Cannot create process\n");
-	    }
-	  printf ("Return: #%d str. PID: %lu\n", WEXITSTATUS (stat), pid[j]);
+//TAKE RESULTS
+	int stat = 0;
+	int status = 0;
+	printf("Simple numbers: ");
+	for (int j = min; j <= max; j++) {
+		status = waitpid(pid[j], &stat, 0);
+		if (status == pid[j]) {
+			if (WEXITSTATUS(stat) == -3) {
+				fprintf(stderr, "Cannot create process\n");
+			}
+			if(WEXITSTATUS(stat) != 0)
+			printf("%d ", WEXITSTATUS(stat));
+			//printf("Return: #%d str. PID: %lu\n", WEXITSTATUS(stat), pid[j]);
+		}
 	}
-    }
+	puts("\n");
+	if (msgctl(msqid, IPC_RMID, NULL)) {
+		perror(strerror(errno));
+		return -2;
+	}
 
-
-
-  fprintf (stderr, "\n(Server)Received exit message\n");
-  if (msgctl (msqid, IPC_RMID, NULL))
-    {
-      perror (strerror (errno));
-      return -2;
-    }
-  
-  fprintf (stderr, "\n(Server)Message queue closed\n");
-  return 0;
+	fprintf(stderr, "\n(Server)Message queue closed\n");
+	return 0;
 }
