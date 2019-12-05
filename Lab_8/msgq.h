165b392be3312683d3410af1aa1e1f5b0035e7dc
Xabirov_Max <Xabirov.2018@nstu.ru>
2019-12-05 14:59:27 +0700

    mod: Lab8 fix msgq.h file

--- Lab_8/msgq.h
+++ Lab_8/msgq.h
@@ -10,13 +10,12 @@
 #include<unistd.h>
 #include<wait.h>
 #include<sys/stat.h>
-#include<sys/stat.h>
 #define MAX_SIZE 1024
-
-
+#define MAX_THREADS 1024
 #define MSGPERM 0600		// msg queue permission
 #define MSGTXTLEN 128		// msg text length
 #define MSGKEY 32769
+
 struct msg_buf
 {
   long mtype;
